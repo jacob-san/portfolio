4b6edcaa8e872463dc3a441308feb0c3072c3949
Sandeep Jacob <jacobsan.dev@gmail.com>
2019-09-07 18:47:34 +0400

    Updates

---
+++ precache-manifest.f96f2278d7ae47291d3c9194339b9a85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f0d93e46476e520e3634f0dc759fdc4",
+    "url": "jacobthedev.club/index.html"
+  },
+  {
+    "revision": "590838317a02462df8fc",
+    "url": "jacobthedev.club/static/css/main.94fbc8b6.chunk.css"
+  },
+  {
+    "revision": "3c9a6581d41e9c9d402c",
+    "url": "jacobthedev.club/static/js/2.d79d9d62.chunk.js"
+  },
+  {
+    "revision": "590838317a02462df8fc",
+    "url": "jacobthedev.club/static/js/main.76beb4e2.chunk.js"
+  },
+  {
+    "revision": "94e4e5c4294347a76c96",
+    "url": "jacobthedev.club/static/js/runtime~main.b40f750f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "jacobthedev.club/static/media/logo.5d5d9eef.svg"
+  }
+]);
