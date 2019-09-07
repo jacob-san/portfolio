6b0d345704c9610a76d975e0a6980780132f2a6d
Sandeep Jacob <jacobsan.dev@gmail.com>
2019-09-07 18:41:31 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Typist from 'react-typist';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Typist cursor={{ show: false }}>
+          <span>Hello, </span>
+          <Typist.Delay ms={1000} />
+          <span>Iam a Dev</span>
+          <Typist.Backspace count={5} delay={400} />
+          <span>Jacob</span>
+          <Typist.Delay ms={1500} />
+
+          <div>
+            <p>I am a Full Stack Developer..</p>
+          </div>
+        </Typist>
       </header>
     </div>
   );
