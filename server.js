3de2fadb99e4a21f70d24b09a434adc6e40c52d4
Sandeep <s.jacob@webdesign366.com>
2019-09-08 12:25:55 +0400

    add env

--- server.js
+++ server.js
@@ -1,29 +1,11 @@
-var express = require("express");
-var history = require("connect-history-api-fallback");
-var app = express();
+const express = require("express");
+const path = require("path");
+const app = express();
 
-// Middleware for serving '/dist' directory
-const staticFileMiddleware = express.static("build/");
+app.use(express.static(path.join(__dirname, "build")));
 
-// 1st call for unredirected requests
-app.use(staticFileMiddleware);
-
-// Support history api
-app.use(
-  history({
-    index: "/build/index.html"
-  })
-);
-
-// 2nd call for redirected requests
-app.use(staticFileMiddleware);
-
-// Handle a get request to an api route
-app.get("*", function(req, res) {
-  // You can retrieve the :id parameter via 'req.params.id's
-  res.sendFile(`${__dirname}/build/index.html`);
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-app.listen(5001, function() {
-  console.log("Server listening on port 5001");
-});
+app.listen(5001);
