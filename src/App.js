f7854f9cb55e2532b017e43c90003350f61a5bee
Sandeep Jacob <jacobsan.dev@gmail.com>
2019-09-14 22:31:44 +0400

    add projects

--- src/App.js
+++ src/App.js
@@ -1,27 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
 import Typist from 'react-typist';
+import projects from './data/projects.json';
+import { images, gifs } from './data/images.js';
 
-function App() {
+const scrollToRef = ref =>
+  ref.current.scrollIntoView({ behavior: 'smooth', block: 'start' });
+
+const App = () => {
+  const projectsRef = React.createRef();
+  useEffect(() => {}, []);
+  const scrollToProjects = event => scrollToRef(projectsRef);
   return (
     <div className="App">
-      <header className="App-header">
-        <Typist cursor={{ show: false }}>
-          <span>Hello, </span>
-          <Typist.Delay ms={1000} />
-          <span>Iam a Dev</span>
-          <Typist.Backspace count={5} delay={400} />
-          <span>Jacob</span>
-          <Typist.Delay ms={1500} />
-
-          <div>
-            <p>I am a Full Stack Developer..</p>
+      <div className="container">
+        <section className="welcome-message">
+          <Typist cursor={{ show: false }}>
+            <div className="hello">
+              <span>Hello, </span>
+              <Typist.Delay ms={1000} />
+              <span>Iam a Full St</span>
+              <Typist.Backspace count={9} delay={400} />
+              <span>Jacob</span>
+              <Typist.Delay ms={1500} />
+            </div>
+            <div>
+              <p className="message-desc">
+                I am a Full Stack Javascript Developer..
+              </p>
+            </div>
+            <div id="scroll" onClick={scrollToProjects}>
+              <div>
+                <span></span>
+                View
+              </div>
+            </div>
+          </Typist>
+        </section>
+        <section ref={projectsRef} className="projects-container">
+          <h2>Projects</h2>
+          <div
+            className="projects"
+            data-aos="fade-up"
+            data-aos-anchor-placement="top-center"
+          >
+            {projects.map((project, index) => (
+              <Card key={project.name} {...project} index={index} />
+            ))}
           </div>
-        </Typist>
-      </header>
+          <div className="more-tag">
+            <div>and more..</div>
+          </div>
+        </section>
+        <section className="contact">
+          <h2>Contact</h2>
+          <div className="info">
+            <p>sandeepmukkadan@gmail.com</p>
+            <p>+971553024874</p>
+          </div>
+        </section>
+      </div>
     </div>
   );
-}
+};
+
+const Card = ({ title, name, link, owner, image, live, gif, index, code }) => {
+  const onMouseOverImage = (e, gif) => (e.currentTarget.src = gifs[code]);
+  const onMouseOutImage = (e, img) => (e.currentTarget.src = images[code]);
+  return (
+    <div className="project-card">
+      <img
+        onMouseOver={onMouseOverImage}
+        onMouseOut={onMouseOutImage}
+        className="project-img"
+        src={images[code]}
+        alt={name}
+      ></img>
+      <div className="card-info">
+        <p className="card-title">{title}</p>
+        <p className="card-description">{name}</p>
+        <p className="card-extra-info">{owner}</p>
+        <div className="card-link">
+          <a href={link || 'javascript:void(0)'} target="_blank">
+            {link ? 'See' : ''}
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default App;
---
+++ src/data/images.js
@@ -0,0 +1,30 @@
+import tralcer from '../assets/tralcer.png';
+import tamm from '../assets/tamm.jpg';
+import ep from '../assets/ep.jpg';
+import yaytrade from '../assets/yaytrade.jpg';
+import spt from '../assets/spt.jpg';
+import itcan from '../assets/itcan.png';
+import tralcerGIF from '../assets/tralcer.gif';
+import tammGIF from '../assets/tamm.gif';
+import epGIF from '../assets/ep.gif';
+import yaytradeGIF from '../assets/yaytrade.gif';
+import sptGIF from '../assets/spt.gif';
+import itcanGIF from '../assets/itcan.gif';
+
+export const images = {
+  tralcer,
+  tamm,
+  ep,
+  yaytrade,
+  spt,
+  itcan
+};
+
+export const gifs = {
+  tralcer: tralcerGIF,
+  tamm: tammGIF,
+  ep: epGIF,
+  yaytrade: yaytradeGIF,
+  spt: sptGIF,
+  itcan: itcanGIF
+};
